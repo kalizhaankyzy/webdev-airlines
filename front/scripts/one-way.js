dc877ea132e166f51ff7d59b9b88c9bb0a1cc69e
Yernur Tengizbay <61284186+YernurTenizbay@users.noreply.github.com>
2022-11-27 11:35:33 +0600

    one way visible work

---
+++ front/scripts/one-way.js
@@ -0,0 +1,9 @@
+function hide(x){
+    if(x==0)
+    document.getElementById('one-way').style.visibility="visible";
+    else
+    document.getElementById('one-way').style.visibility="hidden";
+    
+    return;
+    
+}
