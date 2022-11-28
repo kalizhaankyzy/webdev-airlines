ddf9ccddb43f5ca59c4331ac428eb142aece8452
Yernur Tengizbay <61284186+YernurTenizbay@users.noreply.github.com>
2022-11-29 00:10:23 +0600

    Update checking_flights.php

--- front/checking_flights.php
+++ front/checking_flights.php
@@ -2,22 +2,22 @@
     session_start();
     $order_from=filter_var(trim(strtolower($_POST['order-from'])));
     $order_to=filter_var(trim(strtolower($_POST['order-to'])));
-    $depart_time=$_POST['time-depart'];
-    $return_time=$_POST['time-return'];
+    
+    $depart_time= date($_POST['time-depart']);
+    $return_time=date($_POST['time-return']);
+    
     $mysql=new mysqli("localhost","admin","admin","airlines_new");
     if ($mysql->connect_error) {
         echo "doesnt connect";
         die("Connection failed: " . $mysql->connect_error);
 
       }
-    
-    $result=$mysql->query("SELECT*FROM flight_details WHERE from_city='$order_from' AND to_city='$order_to' AND departure_time='$depart_time'");
+    $result=$mysql->query("SELECT*FROM flight_details WHERE from_city='$order_from' AND to_city='$order_to' AND departure_date='".$depart_time."'");
     $flight=$result->fetch_assoc();
     if(count($flight)==0){
-        var_dump("Invalid");
-        echo $order_from ,$order_to;
+        
         $_SESSION['Inv']=true;
-        // header('Location: ./main.html');
+        header('Location: ./main.html');
         exit;}
     echo "Choose your jet and time";
 
