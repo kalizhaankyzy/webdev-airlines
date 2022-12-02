2f2357ab5c33d13df4315b688428acc5cbede82f
Yernur Tengizbay <61284186+YernurTenizbay@users.noreply.github.com>
2022-12-02 12:24:24 +0600

    Update connection.php

--- front/connection.php
+++ front/connection.php
@@ -1,10 +1,12 @@
 <?php
 $servername = "localhost";
 $database = "airlines";
-$username = "root";
-$password = "qwerty123";
+$username = "admin";
+$password = "admin";
 // Создаем соединение
 $mysql = mysqli_connect($servername, $username, $password, $database);
+$mysql_passid = mysqli_connect($servername, $username, $password, $database);
+$mysql_ticketid = mysqli_connect($servername, $username, $password, $database);
 // Проверяем соединение
 if (!$mysql) {
     die("Connection failed: " . mysqli_connect_error());
